ae1f912db6eec3b804d007795533408f36577f8d
Andreas Fink <andreas@fink.org>
2021-06-22 10:10:03 +0200

    wip

---
+++ Classes/UniversalSMSUtilities/UMSigAddrWithHistory.h
@@ -0,0 +1,14 @@
+//
+//  UMSigAddrWithHistory.h
+//  ulibsmpp
+//
+//  Created by Andreas Fink on 21.06.21.
+//
+
+#import <ulib/ulib.h>
+
+
+@interface UMSigAddrWithHistory : UMStringWithHistory
+
+@end
+
