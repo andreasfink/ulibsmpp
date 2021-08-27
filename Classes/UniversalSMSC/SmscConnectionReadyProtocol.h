2639537e6db3f07a5c10e29bca888f808be05145
Andreas Fink <andreas@fink.org>
2021-08-27 12:34:37 +0200

    ready delegate

---
+++ Classes/UniversalSMSC/SmscConnectionReadyProtocol.h
@@ -0,0 +1,16 @@
+//
+//  SmscConnectionReadyProtocol.h
+//  ulibsmpp
+//
+//  Created by Andreas Fink on 27.08.21.
+//
+
+#import <ulib/ulib.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SmscConnectionReadyProtocol : UMObject
+
+@end
+
+NS_ASSUME_NONNULL_END
